6959d0ec45dc4eaf583cfcfbbdfd95457e9732a4
Simran Farheen <simranf996@gmail.com>
2020-12-20 15:58:52 +0530

    Implemented feature to check for password strength

--- src/main/java/ImageHoster/controller/UserController.java
+++ src/main/java/ImageHoster/controller/UserController.java
@@ -10,9 +10,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 @Controller
@@ -40,9 +43,17 @@ public class UserController {
     //This controller method is called when the request pattern is of type 'users/registration' and also the incoming request is of POST type
     //This method calls the business logic and after the user record is persisted in the database, directs to login page
     @RequestMapping(value = "users/registration", method = RequestMethod.POST)
-    public String registerUser(User user) {
-        userService.registerUser(user);
-        return "redirect:/users/login";
+    public String registerUser(User user, RedirectAttributes redirectAttributes) {
+
+        if(checkPasswordStrength(user.getPassword())) {
+            userService.registerUser(user);
+            return "redirect:/users/login";
+        }
+        else {
+            String error = "Password must contain atleast 1 alphabet, 1 number & 1 special character";
+            redirectAttributes.addAttribute("passwordTypeError", error).addFlashAttribute("passwordTypeError", error);
+            return "redirect:/users/registration";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'users/login'
@@ -78,5 +89,19 @@ public class UserController {
         List<Image> images = imageService.getAllImages();
         model.addAttribute("images", images);
         return "index";
+    }
+
+    //This method checks whether the password supplied by the user
+    //1. Contains 1 alphabet
+    //2. Contains 1 number
+    //3. Contains 1 special character
+    private Boolean checkPasswordStrength(String userPassword){
+        if(userPassword.length()<3)
+            return false;
+        String passwordRegex= "(?=.*[a-z])(?=.*[0-9])(?=.*[^a-z0-9])";
+        Pattern strongPasswordPattern = Pattern.compile(passwordRegex, Pattern.CASE_INSENSITIVE);
+        Matcher passwordMatcher = strongPasswordPattern.matcher(userPassword);
+        if(passwordMatcher.find()) return true;
+        else return false;
     }
 }
